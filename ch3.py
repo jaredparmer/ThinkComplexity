f6d754bf55095a874cebfb1f559e275cfcc21297
jaredparmer <jaredparmer@gmail.com>
2020-10-16 19:14:31 +0200

    added random graph generator for exercise 3.4

--- ch3.py
+++ ch3.py
@@ -5,6 +5,8 @@ import networkx as nx
 import numpy as np
 from collections import deque
 from ch2 import flip, all_pairs
+import random
+import math
 
 
 def adjacent_edges(nodes, halfk):
@@ -30,6 +32,35 @@ def clustering_coefficient(G):
     local clustering coefficients of every node """
     cs = [node_clustering(G, node) for node in G]
     return np.nanmean(cs)
+
+
+""" exercise 3.5 part 1 """
+def make_random_graph(n, k):
+    """ returns a Graph of nodes randomly dispersed in space, each connected to
+    its nearest k neighbors """
+    G = nx.Graph()
+    nodes = range(n)
+    for node in nodes:
+        position = (random.choice(range(-10, 11)),
+                    random.choice(range(-10, 11)))
+        G.add_node(node, pos=position)
+
+    for node in nodes:
+        dist = {}
+        for partner in nodes:
+            node_pos = nx.get_node_attributes(G, 'pos')[node]
+            x1 = node_pos[0]
+            y1 = node_pos[1]
+            partner_pos = nx.get_node_attributes(G, 'pos')[partner]
+            x2 = partner_pos[0]
+            y2 = partner_pos[1]
+            dist[partner] = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+            
+        dist = sorted(dist, key=dist.get)
+        for i in range(k):
+            G.add_edge(node, dist[i])
+
+    return G    
 
 
 """ exercise 3.1 """
@@ -62,6 +93,13 @@ def make_ring_lattice(n, k):
 def make_ws_graph(n, k, p):
     """ returns a Watts-Strogatz graph """
     G = make_ring_lattice(n, k)
+    rewire(G, p)
+    return G
+
+
+""" exercise 3.5 part two """
+def make_ws_random_graph(n, k, p):
+    G = make_random_graph(n, k)
     rewire(G, p)
     return G
 
@@ -169,6 +207,26 @@ def run_one_graph(n, k, p):
     return mpl, cc
 
 
+""" exercise 3.5 part three """
+def run_one_random_graph(n, k, p):
+    """ generates WS variant graph and returns features """
+    ws = make_ws_random_graph(n, k, p)
+    mpl = characteristic_path_length(ws)
+    cc = clustering_coefficient(ws)
+    return mpl, cc
+
+
+""" exercise 3.5 part four (final) """
+def run_random_experiment(ps, n=1000, k=10, iters=20):
+    """ runs experient with WS variant to check robustness of WS results """
+    res = []
+    for p in ps:
+        t = [run_one_random_graph(n, k, p) for _ in range(iters)]
+        means = np.array(t).mean(axis=0)
+        res.append(means)
+    return np.array(res)
+
+
 def shortest_path_dijkstra(G, source):
     dist = {source: 0}
     queue = deque([source])
@@ -201,20 +259,28 @@ def sp_dijkstra_sets(G, source):
     return dist
 
 
-# simple ring lattice with n nodes and k adjacent neighbors for each
-n = 10
-k = 4
-lattice = make_ring_lattice(n, k)
-nx.draw_circular(lattice, node_size = 700, with_labels=True)
+rand = make_random_graph(100, 5)
+nx.draw(rand, nx.get_node_attributes(rand, 'pos'))
+plt.show()
+rewire(rand, 0.3)
+nx.draw(rand, nx.get_node_attributes(rand, 'pos'))
 plt.show()
 
 
-d1 = shortest_path_dijkstra(lattice, 0)
-d2 = sp_dijkstra_sets(lattice, 0)
-print("Dijkstra's algorithm with deque: ", d1)
-print("Dijkstra's algorithm with sets: ", d2)
-print("NetworkX's algorithm: ", nx.shortest_path_length(lattice, 0))
-print(f"d1 == d2: {d1 == d2}")
+# simple ring lattice with n nodes and k adjacent neighbors for each
+##n = 10
+##k = 4
+##lattice = make_ring_lattice(n, k)
+##nx.draw_circular(lattice, node_size = 700, with_labels=True)
+##plt.show()
+##
+##
+##d1 = shortest_path_dijkstra(lattice, 0)
+##d2 = sp_dijkstra_sets(lattice, 0)
+##print("Dijkstra's algorithm with deque: ", d1)
+##print("Dijkstra's algorithm with sets: ", d2)
+##print("NetworkX's algorithm: ", nx.shortest_path_length(lattice, 0))
+##print(f"d1 == d2: {d1 == d2}")
 
 ##regular = make_regular_graph(10, 4)
 ##nx.draw_circular(regular, node_size = 700, with_labels=True)
